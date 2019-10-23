315d9263cd3a9c040959af9dab3575e5a3a87db5
AartiJaiswal <37862199+AartiJaiswal@users.noreply.github.com>
2019-10-23 06:03:29 +0000

    Update dfpowershell.ps1

--- Script/dfpowershell.ps1
+++ Script/dfpowershell.ps1
@@ -1,8 +1,10 @@
-$SecurePassword = "$(key)" | ConvertTo-SecureString -AsPlainText -Force
+$clientID = "43c7a3dd-dab5-45da-8bea-3e3b45dfc425"
+$key = "IEz7S6rkDxMBzpEREd59pAW=H_lHBL/_"
+$SecurePassword = $key | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential `
--argumentlist "$(clientID)", $SecurePassword
+-argumentlist $clientID, $SecurePassword
 $tenantID = "9e9dea74-ace1-4615-a603-b76fa219cf55"
-Connect-AzAccount -Credential $cred -TenantId "$(tenantID)" -ServicePrincipal
+Connect-AzAccount -Credential $cred -TenantId $tenantID -ServicePrincipal
 
 $resourceGroupName = "RogersPOC";
 $dataFactoryName = "RogersDFPOC";
